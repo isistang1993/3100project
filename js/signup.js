03cea10ba3f32cb7ee90292be9c602d7d1e6628c
wlyim <opopocd@gmail.com>
2017-03-20 14:07:55 +0800

    sharon

--- js/signup.js
+++ js/signup.js
@@ -25,7 +25,7 @@ $(window).ready(function(){
     		}
         if(valid_data){
             var xmlhttp = new XMLHttpRequest();
-            var str ="task=verify" + "&" +
+            var str ="task=verify" + "&";
                 str+="username=" + $("#username").val() +"&";
                 str+= "password=" + $("#password").val();
             console.log(str);
@@ -36,6 +36,7 @@ $(window).ready(function(){
             xmlhttp.onreadystatechange = function() {
                 if(xmlhttp.readyState === 4 && xmlhttp.status==200){
                   if(xmlhttp.responseText == "Verified"){
+                      $.update_session();
                       console.log("Account verified");
                   }else{
                       alert("User does not exist or wrong password.");
@@ -45,3 +46,20 @@ $(window).ready(function(){
         }
     });
 });
+
+$.update_user = function(){
+  var xmlhttp = new XMLHttpRequest();
+  var str ="task=session" + "&";
+      str+="username=" + $("#username").val() +"&";
+      str+= "password=" + $("#password").val();
+
+  xmlhttp.open("POST", "PHP/update_user.php", true);
+  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttp.send(str);
+  xmlhttp.onreadystatechange = function() {
+      if(xmlhttp.readyState === 4 && xmlhttp.status==200){
+          console.log(xmlhttp.responseText);
+      }
+  };
+
+}
