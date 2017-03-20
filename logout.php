4b8302f152589ba204f0d37231a1411003a9302e
yu-v-@hotmail.com <isistang1993>
2017-03-20 19:08:06 +0800

    yuyu and sharon

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
 require_once('../Connections/conn.php');
 session_start();
-unset($_SESSION);
+session_unset();
+//header("HTTP/1.1 301 Moved Permanently");
+//header("Location: index.php");
 ?>
+back to <a href="index.php">online store</a>
